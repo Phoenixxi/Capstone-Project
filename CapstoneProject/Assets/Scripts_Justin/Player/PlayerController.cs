e5f89c00dea742acfbe08a2f2885e3236391f36a
JustinMelore <melorejustin@gmail.com>
2025-09-08 19:08:15 -0600

    Began working on basic character movement

---
+++ CapstoneProject/Assets/Scripts_Justin/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Takes in input from the player and performs the associated action. Logic is mostly limited to movement and aiming, with combat logic to be handled by other classes
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float movementSpeed;
+
+
+    private Vector3 movementDirection;
+    private Rigidbody player;
+
+    private void Awake()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    private void OnMove(InputValue input)
+    {
+        Vector2 inputtedDirection = input.Get<Vector2>().normalized;
+        movementDirection.x = inputtedDirection.x;
+        movementDirection.z = inputtedDirection.y;
+    }
+
+    private void FixedUpdate()
+    {
+        player.linearVelocity = movementDirection;
+    }
+}
