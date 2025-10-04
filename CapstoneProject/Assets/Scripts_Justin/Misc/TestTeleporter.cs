754f1c31eff08b054937c75933a7fea781e1f63b
JustinMelore <melorejustin@gmail.com>
2025-10-03 18:53:35 -0600

    Updated teleporters to go to correct scenes

--- CapstoneProject/Assets/Scripts_Justin/Misc/TestTeleporter.cs
+++ CapstoneProject/Assets/Scripts_Justin/Misc/TestTeleporter.cs
@@ -3,15 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class TestTeleporter : MonoBehaviour
 {
+    [SerializeField] private string scene1Name;
+    [SerializeField] private string scene2Name;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player") return;
-        if(SceneManager.GetActiveScene().name == "RuiScene(GYM)")
+        if(SceneManager.GetActiveScene().name == scene1Name)
         {
-            SceneManager.LoadScene("SkylerLevelDesignTempScene");
+            SceneManager.LoadScene(scene2Name);
         } else
         {
-            SceneManager.LoadScene("RuiScene(GYM)");
+            SceneManager.LoadScene(scene1Name);
         }
     }
 }
