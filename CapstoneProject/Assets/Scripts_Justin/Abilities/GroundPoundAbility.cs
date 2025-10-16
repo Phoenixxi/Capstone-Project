1a1d877c247a10c32e0309a4d3b7d681c9c0e697
JustinMelore <melorejustin@gmail.com>
2025-10-15 21:23:49 -0600

    Added coyote time to slam

--- CapstoneProject/Assets/Scripts_Justin/Abilities/GroundPoundAbility.cs
+++ CapstoneProject/Assets/Scripts_Justin/Abilities/GroundPoundAbility.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class GroundPoundAbility : Ability
@@ -7,31 +8,43 @@ public class GroundPoundAbility : Ability
     [SerializeField] private CharacterController entity;
     [SerializeField] private float fallSpeed;
     [SerializeField] private float slamRadius;
+    [SerializeField] private float coyoteTime;
     [SerializeField] private LayerMask slamLayerMask;
-    
+    private float currentCoyoteTime;
+
+
     public override AbilityMovement[] UseAbility(Vector2 horizontalDirection)
     {
         if (currentCooldown > 0f || abilityInUse || entity.isGrounded) return Array.Empty<AbilityMovement>();
         abilityInUse = true;
-        movements[0] = new AbilityMovement(Vector3.down * fallSpeed);
+        currentCoyoteTime = 0f;
+        movements[0] = new AbilityMovement(Vector3.zero);
+        movements[1] = new AbilityMovement(Vector3.down * fallSpeed);
         return movements;
     }
 
     protected override void Update()
     {
         base.Update();
-        if(abilityInUse && entity.isGrounded)
+        if(abilityInUse)
         {
-            movements[0].Complete();
-            abilityInUse = false;
-            Ray sphereRay = new Ray(transform.position, Vector3.down);
-            RaycastHit[] hitEnemies = Physics.SphereCastAll(sphereRay, slamRadius, 0.1f, slamLayerMask);
-            foreach(RaycastHit hitEntity in hitEnemies)
+            if(!entity.isGrounded)
             {
-                Debug.Log($"Hit {hitEntity.transform.gameObject}", hitEntity.transform.gameObject);
-                EntityManager enemy = hitEntity.transform.GetComponent<EntityManager>();
-                if (enemy == null) continue;
-                enemy.TakeDamage(damage, element);
+                currentCoyoteTime += Time.deltaTime;
+                if (currentCoyoteTime >= coyoteTime) movements[0].Complete();
+            } else
+            {
+                movements[1].Complete();
+                abilityInUse = false;
+                Ray sphereRay = new Ray(transform.position, Vector3.down);
+                RaycastHit[] hitEnemies = Physics.SphereCastAll(sphereRay, slamRadius, 0.1f, slamLayerMask);
+                foreach (RaycastHit hitEntity in hitEnemies)
+                {
+                    Debug.Log($"Hit {hitEntity.transform.gameObject}", hitEntity.transform.gameObject);
+                    EntityManager enemy = hitEntity.transform.GetComponent<EntityManager>();
+                    if (enemy == null) continue;
+                    enemy.TakeDamage(damage, element);
+                }
             }
         }
     }
@@ -43,9 +56,9 @@ public class GroundPoundAbility : Ability
         Gizmos.DrawSphere(transform.position, slamRadius);
     }
 
-    private void Awake()
+    protected override void Awake()
     {
         base.Awake();
-        movements = new AbilityMovement[1];
+        movements = new AbilityMovement[2];
     }
 }
