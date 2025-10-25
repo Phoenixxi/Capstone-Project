576219d8908058fd2e1601ab8ddc546e984853e6
Lekopoofball <elsalarnold@gmail.com>
2025-10-24 23:05:59 -0600

    Add Circular Health Bar
    Added health bar sprites and UI elements for the Zoom character. Wrote a script that should update the health bar's radial fill amount and colour in accordance with Zoom's current health relative to max health.

---
+++ CapstoneProject/Assets/Scripts_Leo/PlayerHealthBar.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using lilGuysNamespace;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public EntityManager entityManager;
+    public Image healthBar;
+
+    float maxhp;
+    float hp;
+    float lerpTime;
+    //public Image healthBarBoom, healthBarGloom, healthBarZoom;
+    //public float healthZoom, maxHealthZoom = 100;
+
+    /*
+    CharacterStatus boom, gloom, zoom;
+
+    private class CharacterStatus
+    {
+        public Image hpBar;
+        public float hp, maxhp = 100;
+
+        public CharacterStatus(Image bar)
+        {
+            hpBar = bar;
+        }
+    }
+    */
+    private void Update()
+    {
+        lerpTime = 2f * Time.deltaTime;
+        hp = entityManager.currentHealth;
+
+        updateHPFill();
+        updateColor();
+
+        //healthBarZoom.fillAmount = Mathf.Lerp(healthBarZoom.fillAmount, healthZoom / maxHealthZoom, (2f * Time.deltaTime));
+        //updateHPBarFill(zoom);
+    }
+
+    private void updateHPFill()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, hp / maxhp, lerpTime);
+    }
+
+    private void updateColor()
+    {
+        healthBar.color = Color.Lerp(Color.red, Color.green, hp / maxhp);
+    }
+    /*
+    private void updateHPBarFill(CharacterStatus x)
+    {
+        x.hpBar.fillAmount = Mathf.Lerp(x.hpBar.fillAmount, x.hp / x.maxhp, lerpTime);
+    }
+    */
+}
