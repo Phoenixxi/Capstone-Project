e12b20c120bc80ac5cf4864fb7d5b32b20de8ea4
Phoenixxi <116411018+Phoenixxi@users.noreply.github.com>
2025-11-18 23:15:22 -0700

    Cutscene finished

---
+++ CapstoneProject/Assets/Scripts_Rowan/NextSceneLoader.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class NextSceneLoader : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnEnable()
+    {
+        SceneManager.LoadScene("SkylerLevelDesignTempScene");
+    }
+}
