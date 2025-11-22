139fd0637aef42931d9e8cfc88bdd22b0af66962
Phoenixxi <116411018+Phoenixxi@users.noreply.github.com>
2025-11-21 23:16:39 -0700

    Help UI

--- CapstoneProject/Assets/Scripts_Leo/UIPlayerSwap.cs
+++ CapstoneProject/Assets/Scripts_Leo/UIPlayerSwap.cs
@@ -10,15 +10,83 @@ public class UIPlayerSwap : MonoBehaviour
     [SerializeField] private GameObject gloomHealthRing;
 
     [Header("Locations")]
-    [SerializeField] private Vector3 middleTransform;
+    [SerializeField] private Transform topTransform;
+    [SerializeField] private Transform middleTransform;
+    [SerializeField] private Transform bottomTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        middleTransform = zoomImage.transform.position;
+        topTransform = gloomImage.transform;
+        middleTransform = zoomImage.transform;
+        bottomTransform = boomImage.transform;
+
     }
 
-    public void swapLoc()
+    public void swapImageLocation(int charNum)
     {
-        boomImage.transform.position = middleTransform;
+        int currentState = getCurrentState();
+        if(currentState == -1)
+        {
+            Debug.LogError("Something went wrong with getting the middle image!");
+            return;
+        }
+        
+        switch (currentState)
+        {
+            case 1:
+                Debug.Log("inside case 1");
+                if(charNum == -1)
+                    stateThree();
+                else if(charNum == 1)
+                    stateTwo();
+                break;
+            case 2:
+                Debug.Log("inside case 2");
+                if(charNum == -1)
+                    stateOne();
+                else if(charNum == 1)
+                    stateThree();
+                break;
+            case 3:
+                Debug.Log("inside case 3");
+                if(charNum == -1)
+                    stateTwo();
+                else if(charNum == 1)
+                    stateOne();
+                break;
+        }
+    }
+
+    private int getCurrentState()
+    {
+        if(zoomImage.transform == middleTransform)
+            return 1;
+        if(boomImage.transform == middleTransform)
+            return 2;
+        if(gloomImage.transform == middleTransform)
+            return 3;
+        else
+            return -1;    
+    }
+
+    private void stateOne()
+    {
+        gloomImage.transform.position = topTransform.transform.position;
+        zoomImage.transform.position = middleTransform.transform.position;
+        boomImage.transform.position = bottomTransform.transform.position;
+    }
+
+    private void stateTwo()
+    {
+        zoomImage.transform.position = topTransform.transform.position;
+        boomImage.transform.position = middleTransform.transform.position;
+        gloomImage.transform.position = bottomTransform.transform.position;
+    }
+
+    private void stateThree()
+    {
+        boomImage.transform.position = topTransform.transform.position;
+        gloomImage.transform.position = middleTransform.transform.position;
+        zoomImage.transform.position =  bottomTransform.transform.position;
     }
 }
