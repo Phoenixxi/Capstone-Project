12494c9d91fadb1376474085ad9f5f2e88392b87
JustinMelore <melorejustin@gmail.com>
2025-12-03 23:14:33 -0700

    Subbed out Skyler's scene for Louie's for after the intro cutscene

--- CapstoneProject/Assets/Scripts_Rowan/NextSceneLoader.cs
+++ CapstoneProject/Assets/Scripts_Rowan/NextSceneLoader.cs
@@ -8,6 +8,6 @@ public class NextSceneLoader : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
-        SceneManager.LoadScene("SkylerLevelDesignTempScene");
+        SceneManager.LoadScene("Level01_LouieScene");
     }
 }
