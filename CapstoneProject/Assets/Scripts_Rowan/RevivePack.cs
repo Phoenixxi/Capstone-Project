6a036c729f7912f4b23fda05169d8413388a3266
JustinMelore <melorejustin@gmail.com>
2025-12-04 00:12:22 -0700

    Made new revive stage prefab

--- CapstoneProject/Assets/Scripts_Rowan/RevivePack.cs
+++ CapstoneProject/Assets/Scripts_Rowan/RevivePack.cs
@@ -5,14 +5,22 @@ public class RevivePack : MonoBehaviour
     // Serialized so that designers can adjust in the inspector
     [SerializeField] public float healAmount = 10f; // how much health to restore
 
+    //Note this will likely be removed in the future, just here so there can be some kind of visual indicator of using the station
+    [SerializeField] private GameObject healthSign;
+    [SerializeField] private GameObject healedText;
+    private bool isUsed = false;
+
     private void OnTriggerEnter(Collider other)  
     {
        PlayerController player = other.GetComponentInParent<PlayerController>();
 
-        if (player != null)
+        if (player != null && !isUsed)
         {
             player.HealAllCharacters(healAmount);
-            Destroy(gameObject); // remove the health pack after healing
+            //Destroy(gameObject); // remove the health pack after healing
+            healthSign.SetActive(false);
+            healedText.SetActive(true);
+            isUsed = true;
         }
     }
 }
