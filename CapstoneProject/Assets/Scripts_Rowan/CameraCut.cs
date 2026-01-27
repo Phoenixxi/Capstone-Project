a5dbd9c47e477521163f93882225defe2820920f
Phoenixxi <116411018+Phoenixxi@users.noreply.github.com>
2026-01-26 20:02:46 -0700

    Tutorial camera cut implemented

---
+++ CapstoneProject/Assets/Scripts_Rowan/CameraCut.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+using Unity.Cinemachine;
+using UnityEngine.InputSystem;
+
+public class CameraCut : MonoBehaviour
+{
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject cutSceneCamera;
+    private PlayerInput playerInput;
+
+    void Start()
+    {
+        GameObject Player = GameObject.Find("Player");
+        playerInput = Player.GetComponent<PlayerInput>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        mainCamera.SetActive(false);
+        cutSceneCamera.SetActive(true);
+        playerInput.actions.FindActionMap("Player").Disable();
+        StartCoroutine(waitTime());
+    }
+
+    IEnumerator waitTime()
+    {
+        /// wait 5 seconds
+        yield return new WaitForSeconds(2.5f);
+        // reset cameras
+        mainCamera.SetActive(true);
+        cutSceneCamera.SetActive(false); 
+        StartCoroutine(onReturn());
+        
+    }
+
+    // Gives player controls back
+    IEnumerator onReturn()
+    {
+        yield return new WaitForSeconds(1.8f);
+        playerInput.actions.FindActionMap("Player").Enable();
+        // Destoy this trigger
+        Destroy(gameObject);
+    }
+}
