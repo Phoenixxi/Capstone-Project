307354a42b369b102e97d6a00cfd321e01d93d8c
Phoenixxi <116411018+Phoenixxi@users.noreply.github.com>
2026-01-27 23:07:15 -0700

    made loading screen and UI updates

--- CapstoneProject/Assets/Scripts_Rowan/NextSceneLoader.cs
+++ CapstoneProject/Assets/Scripts_Rowan/NextSceneLoader.cs
@@ -2,12 +2,35 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using Unity.Cinemachine;
 
 public class NextSceneLoader : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
-        SceneManager.LoadScene("Level01_LouieScene");
+        Scene currentScene = SceneManager.GetActiveScene();
+        if(currentScene.name == "IntroCutscene")
+            SceneManager.LoadScene("LoadingScreen");
+
+        if(currentScene.name == "LoadingScreen")
+        {
+            StartCoroutine(LoadAsyncScene());
+        }
     }
+
+    IEnumerator LoadAsyncScene()
+    {
+        yield return new WaitForSeconds(1f);
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync("Level01_LouieScene");
+
+        while(!loadOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+
+
+
 }
