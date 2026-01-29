0618b7ea3bfe7d9e82267e94d95cc5695bead5fb
Ghskljd <ryannip86@gmail.com>
2026-01-28 19:23:04 -0700

    added flashing arrow

--- CapstoneProject/Assets/Scripts_Ryan/SoftLockSpawner/LevelBlocker.cs
+++ CapstoneProject/Assets/Scripts_Ryan/SoftLockSpawner/LevelBlocker.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum ArrowDirection
+{
+    Right, Up, Down, None
+}
+
 public class LevelBlocker : MonoBehaviour
 {
+    [SerializeField] private ArrowDirection arrowDirection;
     [Header("List of mob spawners beyond this level blocker and before the next level blocker")]
     [SerializeField] private List<MobSpawner> MobSpawner;
     [Header("The next scene to go to. \nLeave blank if you just want it to act as a blocker.")]
@@ -13,6 +20,7 @@ public class LevelBlocker : MonoBehaviour
     private List<EntityManager> entityManagers;
     private BoxCollider boxCollider;
     private bool allowedToMoveOn;
+    private FlashingArrow arrow;
 
     void Awake()
     {
@@ -22,6 +30,25 @@ public class LevelBlocker : MonoBehaviour
             allowedToMoveOn = false;
         else
             allowedToMoveOn = true;
+    }
+
+    void Start()
+    {
+        switch(arrowDirection)
+        {
+            case ArrowDirection.Right:
+                arrow = GameObject.Find("RightArrow").GetComponent<FlashingArrow>();
+                break;
+            case ArrowDirection.Up:
+                arrow = GameObject.Find("UpArrow").GetComponent<FlashingArrow>();
+                break;
+            case ArrowDirection.Down:
+                arrow = GameObject.Find("DownArrow").GetComponent<FlashingArrow>();
+                break;
+            case ArrowDirection.None:
+                arrow = null;
+                break;
+        }
     }
 
     public void initializeMobs()
@@ -44,6 +71,10 @@ public class LevelBlocker : MonoBehaviour
         {
             allowedToMoveOn = true;
             boxCollider.isTrigger = true;
+            if(arrow != null)
+            {
+                arrow.StartFlash();
+            }
         }
     }
 
