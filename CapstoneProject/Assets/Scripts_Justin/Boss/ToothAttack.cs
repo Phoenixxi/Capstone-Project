b77d127a3ff8da3a512f8b31588e4d1f9b17f3ad
JustinMelore <melorejustin@gmail.com>
2026-02-10 19:51:38 -0700

    Added WIP tooth attack prefab

---
+++ CapstoneProject/Assets/Scripts_Justin/Boss/ToothAttack.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+/// <summary>
+/// Script that controls the boss attack that causes teeth to shoot out of the ground and hurt the player
+/// </summary>
+public class ToothAttack : BossAttack
+{
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
