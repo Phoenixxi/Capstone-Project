e5aa35fee4c1425cc7bfafd904e608456070fe8a
JustinMelore <melorejustin@gmail.com>
2026-02-20 14:05:28 -0700

    Create foundation for boss attack speedup and applied it to both tentacle attacks

--- CapstoneProject/Assets/Scripts_Justin/Boss/BossAttack.cs
+++ CapstoneProject/Assets/Scripts_Justin/Boss/BossAttack.cs
@@ -8,11 +8,14 @@ public abstract class BossAttack : MonoBehaviour
     [Header("General Attack Settings")]
     [SerializeField] protected int damage;
     [SerializeField] protected bool canRepeat; //Set to true if you don't want the attack to delete itself after finishing
+    [SerializeField] protected float speedupAmount = 1f;
+    [SerializeField] protected bool spawnSpedUp = false;
     public bool IsAttacking { get; protected set; }
 
     protected virtual void Start()
     {
         IsAttacking = false;
+        if (spawnSpedUp) ApplySpeedup();
         if(!canRepeat) Attack();
     }
 
@@ -20,4 +23,9 @@ public abstract class BossAttack : MonoBehaviour
     /// Triggers this attack to occur
     /// </summary>
     public abstract void Attack();
+
+    /// <summary>
+    /// Makes this attack operate at an increased speed
+    /// </summary>
+    public abstract void ApplySpeedup();
 }
