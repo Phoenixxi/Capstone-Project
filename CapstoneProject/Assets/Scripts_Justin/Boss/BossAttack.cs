5855e5ede055af598492c3eb983a0eb84d82b4dd
mystsprit <connorgcolton@gmail.com>
2026-03-02 21:53:15 -0700

    Revert "Merge branch"
    This reverts commit 4aa633f527c4b212b18f79814e244c008acd96a8.

--- CapstoneProject/Assets/Scripts_Justin/Boss/BossAttack.cs
+++ CapstoneProject/Assets/Scripts_Justin/Boss/BossAttack.cs
@@ -8,20 +8,14 @@ public abstract class BossAttack : MonoBehaviour
     [Header("General Attack Settings")]
     [SerializeField] protected int damage;
     [SerializeField] protected bool canRepeat; //Set to true if you don't want the attack to delete itself after finishing
-<<<<<<< Updated upstream
     [SerializeField] protected float speedupAmount = 1f;
     [SerializeField] protected bool spawnSpedUp = false;
-=======
->>>>>>> Stashed changes
     public bool IsAttacking { get; protected set; }
 
     protected virtual void Start()
     {
         IsAttacking = false;
-<<<<<<< Updated upstream
         if (spawnSpedUp) ApplySpeedup();
-=======
->>>>>>> Stashed changes
         if(!canRepeat) Attack();
     }
 
