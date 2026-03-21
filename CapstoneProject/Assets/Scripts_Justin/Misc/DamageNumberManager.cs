94c8ad711dfd968509813e58e544b4aa76aa7228
JustinMelore <melorejustin@gmail.com>
2026-03-21 14:51:30 -0600

    Added the player damage number variant to the DamageNumberManager

--- CapstoneProject/Assets/Scripts_Justin/Misc/DamageNumberManager.cs
+++ CapstoneProject/Assets/Scripts_Justin/Misc/DamageNumberManager.cs
@@ -8,16 +8,21 @@ using ElementType = lilGuysNamespace.EntityData.ElementType;
 public class DamageNumberManager : MonoBehaviour
 {
     [SerializeField] private GameObject damageNumberVFX;
+    [SerializeField] private GameObject playerDamageNumberVFX;
     [SerializeField] private int defaultSize = 10;
     [SerializeField] private int maxSize = 200;
+    [SerializeField] private int playerDefaultSize = 10;
+    [SerializeField] private int playerMaxSize = 100;
     [SerializeField] private float displayTime;
     private int currentSize;
 
     private ObjectPool<DamageNumber> numberPool;
+    private ObjectPool<DamageNumber> playerNumberPool;
 
     private void Awake()
     {
         numberPool = new ObjectPool<DamageNumber>(createFunc: SpawnDamageNumber, actionOnDestroy: DestroyDamageNumber, defaultCapacity: defaultSize, maxSize: maxSize);
+        playerNumberPool = new ObjectPool<DamageNumber>(createFunc: SpawnPlayerDamageNumber, actionOnDestroy: DestroyDamageNumber, defaultCapacity: playerDefaultSize, maxSize: playerMaxSize);
         currentSize = numberPool.CountAll;
     }
 
@@ -29,6 +34,17 @@ public class DamageNumberManager : MonoBehaviour
     {
         DamageNumber number = Instantiate(damageNumberVFX).GetComponent<DamageNumber>();
         number.ObjectPool = numberPool;
+        return number;
+    }
+
+    /// <summary>
+    /// Spawns the player's damage number
+    /// </summary>
+    /// <returns>DamageNumber component of the game object</returns>
+    private DamageNumber SpawnPlayerDamageNumber()
+    {
+        DamageNumber number = Instantiate(playerDamageNumberVFX).GetComponent<DamageNumber>();
+        number.ObjectPool = playerNumberPool;
         return number;
     }
 
@@ -47,10 +63,10 @@ public class DamageNumberManager : MonoBehaviour
     /// <param name="damage">The amount of damage to display</param>
     /// <param name="element">The displayed damage's element</param>
     /// <param name="position">The position the number should be shown at</param>
-    public void ShowDamageNumber(int damage, ElementType element, Vector3 position)
+    public void ShowDamageNumber(int damage, ElementType element, Vector3 position, bool isPlayer = false)
     {
-        Debug.Log($"Called show damage number at position {position}");
-        DamageNumber number = numberPool.Get();
+        //DamageNumber number = numberPool.Get();
+        DamageNumber number = (isPlayer) ? playerNumberPool.Get() : numberPool.Get();
         //TODO Calculate how many other damage numbers there are
         number.ShowDamage(damage, element, displayTime, position);
     }
